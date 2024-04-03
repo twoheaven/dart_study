fb8723cc5d90fafffedb5e25d14b378bc7dd383f
leecheonguk <dlcjsrnr0860@naver.com>
2024-04-04 01:53:45 +0900

    helloworld

---
+++ lib/helloworld/helloworld.dart
@@ -0,0 +1,3 @@
+void helloworld(){
+  print("helloworld");
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/helloworld/helloworld.dart';
 
 void main() {
+  helloworld();
   runApp(const MyApp());
 }
 
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Title',
       theme: ThemeData(
         // This is the theme of your application.
         //
